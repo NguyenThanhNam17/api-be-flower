777d3f35fc6ed104286c983343e9b440676ef1b9
NguyenThanhNam17 <122538702+NguyenThanhNam17@users.noreply.github.com>
2026-02-18 15:49:19 +0700

    init backend project

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import router from "./routers";
+
+dotenv.config();
+
+let mongoURI = process.env.MONGO_URI || "";
+let PORT = process.env.PORT || 5555;
+
+const app = express();
+
+async function connectoMongoDB(connectionString: string) {
+  await mongoose.connect(connectionString);
+  console.log("Connected to MongoDB");
+}
+
+try {
+  connectoMongoDB(mongoURI);
+} catch (err) {
+  console.log("Failed to connect to MongoDB", err);
+}
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+app.use("/", router);
+
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`),
+);
